543be3490a2b4ddcdff92a5e112e46de139b4e1f
shwetasai <shwetasaini250796@gmail.com>
2024-05-30 16:22:01 +0530

    checked comments

--- users/models.py
+++ users/models.py
@@ -2,16 +2,28 @@ from django.contrib.auth.models import AbstractUser, Group, Permission
 from django.db import models
 
 class CustomUser(AbstractUser):
-    is_retailer = models.BooleanField(default=False)
-    is_customer = models.BooleanField(default=False)
+    ROLE_CHOICES = (
+        ('retailer', 'Retailer'),
+        ('customer', 'Customer'),
+    )
+    
+    role = models.CharField(max_length=10, choices=ROLE_CHOICES, default='customer')
 
     class Meta:
         swappable = 'AUTH_USER_MODEL'
-
-class UserGroup(models.Model):
-    customuser = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
-    group = models.ForeignKey(Group, on_delete=models.CASCADE)
-
-class UserPermission(models.Model):
-    customuser = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
-    permission = models.ForeignKey(Permission, on_delete=models.CASCADE)
+    
+    groups = models.ManyToManyField(
+        Group,
+        related_name='customuser_groups',  # Changed to avoid conflicts with the default groups field
+        blank=True,
+        help_text=('The groups this user belongs to. A user will get all permissions '
+                   'granted to each of their groups.'),
+        related_query_name='customuser',
+    )
+    user_permissions = models.ManyToManyField(
+        Permission,
+        related_name='customuser_permissions',  # Changed to avoid conflicts with the default user_permissions field
+        blank=True,
+        help_text='Specific permissions for this user.',
+        related_query_name='customuser',
+    )
