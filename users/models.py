104d1681afcf440935e85bbbaae9a349c7ddd49c
shwetasai <shwetasaini250796@gmail.com>
2024-06-07 19:05:29 +0530

    almost

--- users/models.py
+++ users/models.py
@@ -1,23 +1,57 @@
-from django.contrib.auth.models import AbstractUser, Group, Permission
+from django.contrib.auth.models import AbstractBaseUser, Group, Permission, BaseUserManager,PermissionsMixin
 from django.db import models
 
-class CustomUser(AbstractUser):
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, username, password=None, **extra_fields):
+        if not email:
+            raise ValueError('The Email field must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email, username=username, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        print(f"User {user.email} created with is_email_verified={user.is_email_verified}")
+        return user
+
+    def create_superuser(self, email, username, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self.create_user(email, username, password, **extra_fields)
+
+
+class CustomUser(AbstractBaseUser, PermissionsMixin):
     ROLE_CHOICES = (
         ('retailer', 'Retailer'),
         ('customer', 'Customer'),
     )
 
     email = models.EmailField(unique=True)
+    username = models.CharField(max_length=255)
+    first_name = models.CharField(max_length=30, blank=True)
+    last_name = models.CharField(max_length=30, blank=True)
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    is_superuser = models.BooleanField(default=False)
+    role = models.CharField(max_length=255, default='customer')
+    is_retailer = models.BooleanField(default=False)
+    is_customer = models.BooleanField(default=True)
     is_email_verified = models.BooleanField(default=False)
 
-    def is_verified(self):
-        return self.is_email_verified
-    role = models.CharField(max_length=10, choices=ROLE_CHOICES, default='customer')
-    is_retailer = models.BooleanField(default=False)
-    is_customer = models.BooleanField(default=False)
+
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['username']
-    
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.email
+
+
     class Meta:
         swappable = 'AUTH_USER_MODEL'
     
